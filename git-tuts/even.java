a05eff722e6592033e2874d210905409d5b15eb7
Sushma898 <sushmachintakani@gmail.com>
2025-01-07 10:18:58 +0530

    Add files via upload

---
+++ git-tuts/even.java
@@ -0,0 +1,17 @@
+public class even {
+    public static void main(String[] args)
+    {
+        int n=2;
+        for(int i=2;i<=n;i++)
+        {
+            if(n%2==0)
+            {
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("no");
+            }
+        }
+    }
+    
+}
---
+++ git-tuts/odd.java
@@ -0,0 +1,20 @@
+public class odd
+{
+    public static void main(String[] args)
+    {
+        int n=2;
+        for(int i=2;i<=n;i++)
+        {
+            if(n%2!=0)
+            {
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("no");
+            }
+        }
+    }
+    
+}
+    
+
